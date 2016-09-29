36c2d0151ce337e682be2921529172d6bb23037f
thafsouth96 <thafsuth.adli@gmail.com>
2016-09-29 14:18:58 +0200

    c'est jolie :)

--- news.js
+++ news.js
@@ -18,7 +18,6 @@ function ajouter_recherche()
 		//alert($('#recherches-stockees').children().last().children().last().attr("src")) ;
 		$('#recherches-stockees p:last-child').children().last().attr("onclick","supprimer_recherche(this)");
 		var e_textJson = JSON.stringify(recherches);
-		alert(e_textJson);
 		$.cookie("recherches",e_textJson, {expires : 1000});
 	}
 
@@ -29,8 +28,8 @@ function supprimer_recherche(e)
 	var e_text = e.parentElement.firstChild.innerHTML;
 	e.parentElement.remove();
 	recherches.splice(recherches.indexOf(e_text),1);
-
-
+	var e_textJson = JSON.stringify(recherches);
+	$.cookie("recherches",e_textJson, {expires : 1000});
 }
 
 
@@ -39,27 +38,53 @@ function selectionner_recherche(e)
 	var e_text = e.parentElement.firstChild.innerHTML;
 	$('#zone_saisie').val(e_text);
 	recherche_courante = e_text ;
-
 }
 
 
 function init()
 {
+	var cookie = $.cookie("recherches");
+	if (cookie!=null){
+		recherches = $.parseJSON(cookie);
+		for (var i=0; i<recherches.length; i++){
+			$('#recherches-stockees').append('<p class= "titre-recherche"><label>'+recherches[i]+'</label><img src="croix30.jpg" class ="icone-croix"/></p>');
+			$('#recherches-stockees').children().last().children().first().attr("onclick","selectionner_recherche(this)");
+			$('#recherches-stockees p:last-child').children().last().attr("onclick","supprimer_recherche(this)");
+		}
+	}
+
 
 }
 
 
 function recherche_nouvelles()
 {
-
+	$('#resultats').empty();
+	$('#wait').css("display","block") ;
+	var data = $('#zone_saisie').val();
+	$.ajax({
+		url : 'search.php?data='+data,
+		type : 'GET',
+		//async : false, //Demander à M. Brouard
+		dataType : 'json',
+		success : maj_resultats
+	});
 
 }
 
 
 function maj_resultats(res)
 {
-
-
+	$('#wait').css("display","none");
+	recherche_courante_news = res; // JQuery parse automatiquement le JSON renvoyé par le serveur s'il est valide
+	for(var i=0; i<recherche_courante_news.length; i++){
+		$('#resultats').append(decodeEntities(
+			'<p class="titre_result"><a class="titre_news" href="'+recherche_courante_news[i].url
+			+'" target="_blank">'+recherche_courante_news[i].titre
+			+'</a><span class="date_news">'+format(recherche_courante_news[i].date)
+			+'</span><span class="action_news" onclick="sauve_news(this)"><img src="horloge15.jpg"/></span></p>')
+		)
+	}
 }
 
 
